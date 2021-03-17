73678d47173823930fb4dcf58cc7c740d8a29b9d
Robin Ferm <Frolleferm@hotmail.com>
2021-03-17 14:44:44 +0100

    Added a simple selectable menu

---
+++ Source/SpacePort/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePort
+{
+    class Menu
+    {
+        public static object ShowMenu(string info, object[] options)
+        {
+            if (options.Length <= 0 || options == null)
+            {
+                throw new ArgumentException("Options is empty.");
+            }
+
+            Console.CursorVisible = false;
+            int selected = 0;
+
+            HighlightMenuOption(info, options, selected);
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            while(key != ConsoleKey.Enter)
+            {
+                if (key == ConsoleKey.UpArrow && selected > 0)
+                {
+                    selected--;
+                    HighlightMenuOption(info, options, selected);
+                }
+                else if (key == ConsoleKey.DownArrow && selected < options.Length -1)
+                {
+                    selected++;
+                    HighlightMenuOption(info, options, selected);
+                }
+
+                key = Console.ReadKey().Key;
+            }
+
+            return options[selected];
+
+        }
+        public static void HighlightMenuOption(string info, object[] options, int index)
+        {
+            //Clear the console so it doesn't print the new values on new lines, but instead replaces current values with new values on respective line
+            Console.Clear();
+
+            //print info once again
+            Console.WriteLine(info);
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                //if i equals the index value we are highlighting, print it in green color with an arrow to show that THIS is the value we are on
+                if (i == index)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("> " + options[i]);
+                    //reset text color back to gray
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                //else simply print the value
+                else
+                {
+                    Console.WriteLine(options[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Source/SpacePort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpacePort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.ShowMenu("Välkommen", new string[] { "Parkera", "Betala" });
+            
+        }
+    }
+}
