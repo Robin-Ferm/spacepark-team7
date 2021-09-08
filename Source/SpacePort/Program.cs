46b89b549d5cb6c91d38bc940f82d7bab6ac8021
Kristianjimmefors <Kristian.jimmefors@gmail.com>
2021-09-08 11:17:56 +0200

    removed esh

--- Source/SpacePort/Program.cs
+++ Source/SpacePort/Program.cs
@@ -6,7 +6,7 @@ namespace SpacePort
     {
         static void Main(string[] args)
         {
-            var alt = new string[] { "Park", "Pay for parking", "Exit", "Test", "esh" };
+            var alt = new string[] { "Park", "Pay for parking", "Exit", "Test" };
             object selected = alt;
             while (selected.ToString() != "Exit")
             {
